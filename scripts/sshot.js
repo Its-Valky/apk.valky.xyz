6654a9150ce50b8cad20bbe7bcc49f2f5febd252
Valky <53298849+Its-Valky@users.noreply.github.com>
2020-03-07 14:53:42 +0100

    fix

--- scripts/sshot.js
+++ scripts/sshot.js
@@ -2,7 +2,7 @@
 
 
 // take screenshot
-$("button[name='submit']").on("click", function(){
+$("button[name='screenshot']").on("click", function(){
     
     document.querySelector('a-scene').components.screenshot.capture('perspective');
 
